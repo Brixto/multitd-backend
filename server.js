f73d1e368ee075cb851981e016f06b004db2eeb2
Brixto <jonasfroeber@gmail.com>
2019-04-30 16:12:37 +0200

    exec spawn

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
+const { exec } = require('child_process');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-app.listen(3000, function() {
+app.get('/spawn', (req, res) => {
+    console.log('spawn');
+    exec('/src/multitd-build/multitd.x86_64 ', (err, stdout, stderr) => {
+        console.log(stderr);
+        console.log('spawned');
+        if (err) {
+            console.error(`exec error: ${err}`);
+            return;
+        }
+
+        console.log(`${stdout}`);
+    });
+});
+
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });

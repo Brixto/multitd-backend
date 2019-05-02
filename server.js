0be17493f8b2e0ac37b4c8c950ab38a67c3ac28c
Brixto <jonasfroeber@gmail.com>
2019-05-02 17:23:30 +0200

    spawn unity

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 var express = require('express');
 var app = express();
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
+const psList = require('ps-list');
+const getPort = require('get-port');
 
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-app.get('/spawn', (req, res) => {
+app.get('/spawn', async (req, res) => {
     console.log('spawn');
-    exec('/src/multitd-build/multitd.x86_64 ', (err, stdout, stderr) => {
-        console.log(stderr);
-        console.log('spawned');
-        if (err) {
-            console.error(`exec error: ${err}`);
-            return;
-        }
-
-        console.log(`${stdout}`);
+    var port = await getPort({ port: getPort.makeRange(3001, 3200) })
+    const child = spawn('/src/multitd-build/multitd.x86_64', ['-batchmode', '-nographics', '-port', port]);
+    child.stdout.on('data', (data) => {
+        console.log(`${data}`);
     });
+
+    child.stderr.on('data', (data) => {
+        console.error(`child stderr:\n${data}`);
+    });
+});
+
+app.get('/ps', async (req, res) => {
+    var pslist = await psList();
+    pslist = pslist.filter(ps => ps.name === 'multitd.x86_64')
+    res.send(pslist)
 });
 
 app.listen(3000, function () {

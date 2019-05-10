e7a51d77904a06bcdd934941b60b28f0e4eb6c69
Brixto <jonasfroeber@gmail.com>
2019-05-10 15:53:48 +0200

    socketio

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 const { spawn } = require('child_process');
 const psList = require('ps-list');
 const getPort = require('get-port');
+
+var servers = [];
 
 app.get('/', function (req, res) {
     res.send('Hello World');
@@ -12,6 +16,9 @@ app.get('/spawn', async (req, res) => {
     console.log('spawn');
     var port = await getPort({ port: getPort.makeRange(3001, 3200) })
     const child = spawn('/src/multitd-build/multitd.x86_64', ['-batchmode', '-nographics', '-port', port]);
+
+    servers.push({ status: 'pending', port: port });
+
     child.stdout.on('data', (data) => {
         console.log(`${data}`);
     });
@@ -28,6 +35,25 @@ app.get('/ps', async (req, res) => {
     res.send(pslist)
 });
 
-app.listen(3000, function () {
+app.get('/servers', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+    console.log('instance connected');
+    socket.on('started', (port) => {
+        servers.forEach(s => {
+            if (s.port === port) s.status = 'started';
+        })
+        socket.emit('serverupdate', servers);
+        console.log('instance started');
+    });
+
+    socket.on('getservers', () => {
+        socket.emit('serverupdate', servers);
+    });
+})
+
+http.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });

d305a52ca3f84bade99d269256a14f165f9ecf88
Brixto <jonasfroeber@gmail.com>
2019-05-17 16:27:55 +0200

    networking stuff

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 app.get('/spawn', async (req, res) => {
     var port = await getPort({ port: getPort.makeRange(3001, 3200) })
     const child = spawn('/src/multitd-build/multitd.x86_64', ['-batchmode', '-nographics', '-port', port]);
-    servers.push({ server: child, status: 'starting up', port: port });
+    servers.push({ status: 'starting up', port: port });
     io.sockets.emit('serverupdate', servers);
     console.log('started server on port ' + port);
 
@@ -26,27 +26,29 @@ app.get('/servers', (req, res) => {
 });
 
 io.on('connection', socket => {
-    console.log('instance connected');
     socket.on('started', (port) => {
-        getServer(port).status = 'waiting for connections';
-        socket.emit('serverupdate', servers);
-        console.log('instance started');
+        console.log('started on port ' + port);
+        var server = getServer(port);
+        server.status = 'waiting for connections';
+        io.sockets.emit('serverupdate', servers);
     });
 
     socket.on('getservers', () => {
         socket.emit('serverupdate', servers);
     });
 
+    socket.on('playerconnected', (numplayers) => {
+        console.log('player connected' + numplayers);
+    })
+
     // socket.on('gameend', (port) => {
     //     server = getServer(port).child;
     //     server.kill();
     // });
-})
+});
 
 function getServer(port) {
-    servers.forEach(server => {
-        if (server.port === port) return server;
-    });
+    return servers.find(s => s.port.toString() === port);
 }
 
 http.listen(3000, function () {

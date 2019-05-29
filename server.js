bb23eef4a1ee3fb47ce635fe5d9ef6b4fb70efcb
Brixto <jonasfroeber@gmail.com>
2019-05-29 17:20:03 +0200

    randommized

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ const Vector2 = require('./vector2');
 var spawnPoint = new Vector2(0, 0);
 var destination = new Vector2(8, 3);
 
-var grid = new PF.Grid(16, 16);
-var finder = new PF.AStarFinder();
-var path = finder.findPath(spawnPoint.x, spawnPoint.y, destination.x, destination.y, grid);
+var grid = new PF.Grid(18, 28);
+var finder = new PF.AStarFinder({ allowDiagonal: true });
+//var path = finder.findPath(spawnPoint.x, spawnPoint.y, destination.x, destination.y, grid);
 
-const moveFPS = 20;
+const moveFPS = 60;
+const updateTime = 10;
 
 // for (i = 0; i < path.length; ++i) {
 //     console.log(path[i][0]);
@@ -27,41 +28,61 @@ const moveFPS = 20;
 var minions = [];
 
 spawnMinion();
-setInterval(spawnMinion, 5000);
+setInterval(spawnMinion, 1000);
 setInterval(moveLoop, 1000 / moveFPS);
+
+setInterval(updateLoop, 1000 / updateTime);
 
 function pathToVector(path) {
     return new Vector2(path[0], path[1]);
 }
 
 function spawnMinion() {
+    var rx = Math.floor(Math.random() * 18);
+    var ry = Math.floor(Math.random() * 28);
+    var spawnPoint = new Vector2(rx, ry);
+
+    var dx = Math.floor(Math.random() * 18);
+    var dy = Math.floor(Math.random() * 28);
+    var destination = new Vector2(dx, dy);
+
+    var path = finder.findPath(spawnPoint.x, spawnPoint.y, destination.x, destination.y, grid);
+
     var minion = {
         id: shortid.generate(),
         ...spawnPoint,
         i: 0,
+        path,
     };
     minions.push(minion);
 }
 
+function updateLoop() {
+    if (minions.length > 0) {
+        var clonedArray = JSON.parse(JSON.stringify(minions))
+        clonedArray.forEach(m => delete m.path);
+        console.log(clonedArray);
+        io.sockets.emit('update', clonedArray);
+    }
+}
+
 function moveLoop() {
     for (var minion of minions) {
-        if (minion.i < path.length) {
-            var nextPoint = pathToVector(path[minion.i])
+        if (minion.i < minion.path.length) {
+            var nextPoint = pathToVector(minion.path[minion.i])
             Vector2.moveTowards(minion, nextPoint, 0.1);
 
             if (minion.x == nextPoint.x && minion.y == nextPoint.y) {
                 minion.i += 1;
             }
         } else {
+            // reached end
             var index = minions.indexOf(minion);
             if (index > -1) {
+                io.sockets.emit('end', minion.id);
                 minions.splice(index, 1);
             }
         }
-    }
-    if (minions.length > 0) {
-        console.log(minions);
-        io.sockets.emit('update', minions);
     }
 }
 
